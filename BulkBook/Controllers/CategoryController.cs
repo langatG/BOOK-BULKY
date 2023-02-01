3c672d8b2a047729c04b34464f89d52a1d9f9404
LANGATG <langatgilbert1115@Gmail.com>
2023-02-01 22:06:47 +0300

    uploaded new project and some server side validation added

---
+++ BulkBook/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using BulkBook.Data;
+using BulkBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categorylist = _db.Categories;
+            return View(categorylist);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           return View(obj);
+        }
+    }
+}
---
+++ BulkBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkBook.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+       public DbSet<Category> Categories { get; set; }
+    }
+}
