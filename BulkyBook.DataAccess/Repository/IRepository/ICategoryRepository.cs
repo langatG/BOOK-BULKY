4620fb8162dc5b7517f3d968af93279bbcc6a99d
LANGATG <langatgilbert1115@Gmail.com>
2023-02-03 23:21:30 +0300

    Added Repository pattern

---
+++ BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using BulkBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        //T -> Category
+        void Update(Category obj);
+        void Save();
+    }
+}
